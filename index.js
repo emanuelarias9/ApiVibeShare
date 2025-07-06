8a3c484b2e645d8ddc7a1c7c3ebda4db0de1aad5
Emanuel Arias <Emanuelarias9@gmail.com>
2025-07-06 17:27:54 -0500

    Set up Express server with CORS and JSON body parsing

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 // importar dependencias
 const connection = require("./database/connection.js");
+const express = require("express");
+const cors = require("cors");
 
 //conexion
 connection();
+
+// crear el servidor
+const app = express();
+const port = 3900;
+
+// Middlewares
+
+//configurar cors
+app.use(cors());
+
+// convertir body a json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//ruta de prueba
+app.get("/Test", (req, res) => {
+  return res.status(200).json({
+    message: "API VibeShare funcionando correctamente",
+  });
+});
+
+// Iniciar servidor y escuchar rutas
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
