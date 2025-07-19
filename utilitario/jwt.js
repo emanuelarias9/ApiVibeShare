82af6920587e3ae8ac59f1c6f17ce00308b92e87
Emanuel Arias <Emanuelarias9@gmail.com>
2025-07-18 21:42:10 -0500

    Remove unnecessary user fields from JWT payload

--- utilitario/jwt.js
+++ utilitario/jwt.js
@@ -4,11 +4,7 @@ const moment = require("moment");
 const createToken = (user) => {
   const payload = {
     id: user._id,
-    username: user.username,
-    nick: user.nick,
-    email: user.email,
     role: user.role,
-    image: user.image,
     iat: moment().unix(), // Issued at time
     exp: moment().add(1, "days").unix(),
   };
